02abe4266d45ef5b914285aa14c0b432bc1e6214
ThanhPhong <nguyenthanhphong2912002@gmail.com>
2025-11-27 15:08:24 +0700

    fix: css1.css

--- views/index.php
+++ views/index.php
@@ -67,7 +67,7 @@
 
 	<!-- lien he , gioi thieu -->
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-	<link href='https://fonts.googleapis.com/css?family=Lato:400,900,700,300' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Roboto:400,900,700,300' rel='stylesheet' type='text/css'>
 	<link href='https://fonts.googleapis.com/css?family=Bree+Serif' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" href="public/css/gioithieu.css">
 	<!-- all css here -->
